0ad131bb0c0687613594ba4dd189de2cb62394c0
Alex Antonov <kaburbundokel11g@inbox.ru>
2017-11-12 07:57:08 +0300

    ex4

---
+++ ex4.py
@@ -0,0 +1,11 @@
+cars = 100
+space_in_the_car = 4
+carpool_capacity = cars // space_in_the_car
+
+# print("We can drive:", carpool_capacity)
+# print("{0}is the carpool capacity".format(carpool_capacity))
+# print(0.1 + 0.2 == 0.3)
+# print(.1 == 0.1)
+
+x = .1
+print(x.as_integer_ratio())
