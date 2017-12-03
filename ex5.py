3b37ad7ab2ba46e4e3f1779a826fcbc8d90f6404
Alex Antonov <kaburbundokel11g@inbox.ru>
2017-12-03 14:59:27 +0300

    ex5

---
+++ ex5.py
@@ -0,0 +1,9 @@
+my_name = "Alex Antonov"
+my_height = 187
+
+centimeters_in_inch = 2.54
+
+my_inches_height = my_height / centimeters_in_inch
+result = int(my_inches_height)
+
+print ("Let's talk about {}, he is {:d} inches height".format(my_name, result))
