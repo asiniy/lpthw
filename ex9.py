3d263acba834c97b13191945001816f499c094e8
Alex Antonov <kaburbundokel11g@inbox.ru>
2017-12-03 20:35:18 +0300

    ex9

---
+++ ex9.py
@@ -0,0 +1,11 @@
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay"
+
+print("Here are the days", days)
+print("Here are the months", months)
+
+print("""
+There is something multiline here
+Jan
+Mon
+""")
