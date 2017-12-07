5c8ee6fcea178f6837c4a576e24cb0ef2db31abc
Alex Antonov <kaburbundokel11g@inbox.ru>
2017-12-07 15:44:55 +0300

    ex18

---
+++ ex18.py
@@ -0,0 +1,11 @@
+def p2(*args):
+    arg1, arg2 = args
+    print("arg1: {} & arg2: {}".format(arg1, arg2))
+
+def p1(arg):
+    print("arg1: {}".format(arg))
+
+def nothing():
+    pass
+
+print(nothing())
