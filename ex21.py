00fc4b64f9269bf5af92accf4d9e33d0336dc85b
Alex Antonov <kaburbundokel11g@inbox.ru>
2017-12-09 08:57:45 +0300

    ex21

---
+++ ex21.py
@@ -0,0 +1,10 @@
+def add(a, b):
+    print("Adding {} and {}".format(a, b))
+    return a + b
+
+def divide(a, b):
+    print("Dividing {} and {}".format(a, b))
+    return a / b
+
+x = divide(30.2, 5)
+print(x)
