ca9ced8dbd5a792534ded6da02d83424300e2637
Alex Antonov <kaburbundokel11g@inbox.ru>
2017-12-09 18:22:33 +0300

    ex29

---
+++ ex29.py
@@ -0,0 +1,17 @@
+people = 20
+cats = 30
+dogs = 15
+
+if people < cats:
+    print("Too many cats")
+
+if people < dogs:
+    print("I'll never happen")
+
+if people > dogs:
+    print("Show must go on!")
+
+dogs += 5
+
+if people == dogs:
+    print("MAGA!")
