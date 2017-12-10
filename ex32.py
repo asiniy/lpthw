61b7d3bf69902ebdbe91442e82d8da0c1cc85556
Alex Antonov <kaburbundokel11g@inbox.ru>
2017-12-10 08:36:03 +0300

    ex32

---
+++ ex32.py
@@ -0,0 +1,12 @@
+count = [1, 2, 3, 4, 5]
+
+for number in count:
+    print("This is number {}".format(number))
+
+elements = []
+
+for i in range(0, 6):
+    elements.append("new i is: {}".format(i))
+
+for element in elements:
+    print(element)
